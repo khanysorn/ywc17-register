7a14e7f8f9ecb8afbedaa8e5419bdca396ec9945
Wiput Pootong <wiput.pootong@gmail.com>
2019-08-16 00:17:46 +0700

    :ambulance: ant css import

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Loadable from 'react-loadable'
 import { Route, Router } from 'react-router'
 import { createGlobalStyle } from 'styled-components'
 
-import 'antd/dist/antd.less'
+import 'antd/dist/antd.css'
 import './index.css'
 
 import store from './stores'
