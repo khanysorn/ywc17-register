336d938e0032a0a3c8bfc990df1fba09bfccd8f4
Wiput Pootong <wiput.pootong@gmail.com>
2019-08-17 00:01:03 +0700

    :sparkles: add getProfile to auth

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,8 +1,10 @@
 import { message } from 'antd'
 import * as firebase from 'firebase/app'
 import { action, observable } from 'mobx'
-import { fetch } from '../utils/fetch'
+import { fetch, fetchWithToken } from '../utils/fetch'
 import { auth } from '../utils/firebase'
+import history from '../utils/history'
+import { saveToken } from '../utils/token-helper'
 
 class Auth {
   @observable loading: boolean = false
@@ -26,9 +28,36 @@ class Auth {
     const login = await fetch('auth/login', { accessToken }, 'POST')
 
     if (login.status === 'success') {
-      message.success('เข้าสู่ระบบสำเร็จ')
-      this.loading = false
+      saveToken(login.payload.token)
+
+      this.getProfile()
     }
+  }
+
+  @action
+  async getProfile() {
+    const getProfile = await fetchWithToken('users/me', {}, 'GET')
+
+    if (getProfile.status !== 'success') {
+      message.error('Something went wrong!')
+
+      this.loading = false
+
+      return
+    }
+
+    message.success('เข้าสู่ระบบสำเร็จ')
+
+    if (getProfile.payload.status === 'completed') {
+      history.push(`/completed`)
+      message.warning('คุณได้ทำการส่งใบสมัครแล้ว')
+      this.loading = false
+      return
+    } else {
+      history.push(`/step/${getProfile.payload.step}`)
+    }
+
+    this.loading = false
   }
 }
 
