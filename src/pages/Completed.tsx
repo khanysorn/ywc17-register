bc2080741d66a98862f5af9d260120361efdf9d4
Wiput Pootong <wiput.pootong@gmail.com>
2019-08-17 02:08:39 +0700

    :green_heart:

--- src/pages/Completed.tsx
+++ src/pages/Completed.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Icon, Spin } from 'antd'
+import { Avatar } from 'antd'
 import { observer, useObservable } from 'mobx-react-lite'
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
